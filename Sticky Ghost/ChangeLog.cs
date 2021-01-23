890ca736893c2ff65ed0f978839a1842b210b1c8
Ven <rein.akihisa@gmail.com>
2021-01-23 21:05:47 +0800

    Sticky Ghost
    main code

---
+++ Sticky Ghost/ChangeLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sticky_Ghost
+{
+    public partial class ChangeLog : UserControl
+    {
+        public ChangeLog()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBoard_Click(object sender, EventArgs e)
+        {
+            {
+                System.Diagnostics.Process.Start("https://github.com/seizue/Sticky-Ghost/blob/master/Changelog");
+            }
+        }
+    }
+}
